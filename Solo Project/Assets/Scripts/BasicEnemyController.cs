2dae54a0df9dde1a24ac17d3fc81685a3409063d
AetherWolf26 <evb12906@gmail.com>
2025-10-28 13:20:45 -0700

    co

--- Solo Project/Assets/Scripts/BasicEnemyController.cs
+++ Solo Project/Assets/Scripts/BasicEnemyController.cs
@@ -1,59 +1,19 @@
 using UnityEngine;
 using UnityEngine.AI;
+
 public class BasicEnemyController : MonoBehaviour
 {
-    PlayerController player;
-    Animator myAnim;
-
-    [Header("Logic")]
     NavMeshAgent agent;
-    public bool isFollowing = false;
-
-    [Header("Enemy Stats")]
-    public int health = 5;
-    public int maxHealth = 5;
-
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        myAnim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
+        agent.destination = GameObject.Find("Player").transform.position;
 
-        else
-        {
-            if (isFollowing)
-            {
-                agent.isStopped = false;
-                agent.destination = player.transform.position;
-                myAnim.SetBool("isAttacking", true);
-            }
-            else
-            {
-                agent.isStopped = true;
-                myAnim.SetBool("isAttacking", false);
-            }
-        }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "proj")
-        {
-            health--;
-            Destroy(collision.gameObject);
-        }
-    }
-
-
 }
